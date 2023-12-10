dd8c1d11123dacd923ef8a36d982d3582cf05470
galimalki <galim@r-stealth.com>
2023-12-10 11:33:14 +0200

    unit 5 fix

--- unit05-lightning/exercises/1_lightning-regression/training.py
+++ unit05-lightning/exercises/1_lightning-regression/training.py
@@ -8,8 +8,6 @@ import torch
 from shared_utilities import LightningModel, AmesHousingDataModule, PyTorchMLP
 from watermark import watermark
 
-# Unit 5.5. Organizing Your Data Loaders with Data Modules
-
 if __name__ == "__main__":
 
     print(watermark(packages="torch,lightning", python=True))
@@ -38,20 +36,19 @@ if __name__ == "__main__":
         f" | Test MSE {test_mse:.2f}"
     )
 
-metrics = pd.read_csv(f"{trainer.logger.log_dir}/metrics.csv")
+# Load metrics from a CSV file
+metrics_path = f"{trainer.logger.log_dir}/metrics.csv"
+metrics_df = pd.read_csv(metrics_path)
 
-aggreg_metrics = []
-agg_col = "epoch"
-for i, dfg in metrics.groupby(agg_col):
-    agg = dict(dfg.mean())
-    agg[agg_col] = i
-    aggreg_metrics.append(agg)
+# Aggregate metrics by epoch and calculate the mean for each epoch
+epoch_aggregated = metrics_df.groupby("epoch").mean().reset_index()
 
-df_metrics = pd.DataFrame(aggreg_metrics)
-df_metrics[["train_loss", "val_loss"]].plot(
-    grid=True, legend=True, xlabel="Epoch", ylabel="Loss"
-)
-df_metrics[["train_mse", "val_mse"]].plot(
-    grid=True, legend=True, xlabel="Epoch", ylabel="MSE"
-)
+# Plot training and validation loss
+epoch_aggregated.plot(x="epoch", y=["train_loss", "val_loss"],
+                      grid=True, legend=True, xlabel="Epoch", ylabel="Loss")
+
+# Plot training and validation mean squared error (MSE)
+epoch_aggregated.plot(x="epoch", y=["train_mse", "val_mse"],
+                      grid=True, legend=True, xlabel="Epoch", ylabel="MSE")
+
 plt.show()
